5113d1d821810fcfcfeab80a9cf3f95e0701f10f
Shahriar Azad <140730820+Shahriar-Azad@users.noreply.github.com>
2024-04-22 23:33:14 +0600

    Update dbConfig.php

--- CSE391_Assignment_03/CSE391-Assignment03-master/config/dbConfig.php
+++ CSE391_Assignment_03/CSE391-Assignment03-master/config/dbConfig.php
@@ -1,9 +1,9 @@
 <?php
 
 $server_address = "localhost";              	//If you connecting remotely to this database you need to set the hostname of your server and you need to allow access to your ip from server
-$database_name = "tuhinmri_cse391";   	    //Set your database name
-$username = "tuhinmri_tuhin";		    //Set your database username
-$password = "012Dark345";		    //Set database password
+$database_name = "391_db";   	    //Set your database name
+$username = "xyz";		    //Set your database username
+$password = "12345";		    //Set database password
 
 
 // Create connection
